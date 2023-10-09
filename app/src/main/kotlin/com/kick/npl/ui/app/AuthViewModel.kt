81067ded38ba9f5b8131404e1f753fbe5410fefc
uzun <hyjhyj0901@pusan.ac.kr>
2023-10-09 23:25:33 +0900

    merge it

--- app/src/main/kotlin/com/kick/npl/ui/app/AuthViewModel.kt
+++ app/src/main/kotlin/com/kick/npl/ui/app/AuthViewModel.kt
@@ -3,39 +3,44 @@ package com.kick.npl.ui.app
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.kick.npl.data.local.AuthLocalDataSource
+import com.kick.npl.data.local.TokenInfo
 import com.kick.npl.ui.util.UiState
 import com.kick.npl.ui.util.getIdleUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AuthViewModel @Inject constructor() : ViewModel() {
+class AuthViewModel @Inject constructor(
+    private val localDataSource: AuthLocalDataSource,
+) : ViewModel() {
 
     private val _result = MutableStateFlow(getIdleUiState<Unit>())
     val result = _result.asStateFlow()
+
     fun loginWithKakaoTalk(context: Context) {
-        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
-            viewModelScope.launch(Dispatchers.IO) {
+        if (isKakaoTalkLoginAvailable(context)) {
+            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
                 if (error != null) {
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@launch
-                    } else {
-                        _result.update { UiState.Error("로그인에 실패하였습니다") }
+                    if ((error is ClientError && error.reason == ClientErrorCause.Cancelled).not()) {
+                        error.printStackTrace()
+                        onSignInFailure()
                     }
-                } else {
-                    _result.update { UiState.Success(Unit) }
+                } else if(token != null) {
+                    onSignInSuccess(token)
                 }
             }
+        } else {
+            loginWithKakaoAccount(context)
         }
     }
 
@@ -43,13 +48,34 @@ class AuthViewModel @Inject constructor() : ViewModel() {
         UserApiClient.instance.loginWithKakaoAccount(context) { token, error ->
             viewModelScope.launch(Dispatchers.IO) {
                 if (error != null) {
-                    _result.update { UiState.Error("로그인에 실패하였습니다") }
+                    error.printStackTrace()
+                    onSignInFailure()
                 } else if (token == null) {
-                    _result.update { UiState.Error("사용자 토큰이 존재하지 않습니다") }
+                    onSignInFailure("사용자 토큰이 존재하지 않습니다")
                 } else {
-                    _result.update { UiState.Success(Unit) }
+                    onSignInSuccess(token)
                 }
             }
         }
+    }
+
+    private fun isKakaoTalkLoginAvailable(context: Context): Boolean {
+        return UserApiClient.instance.isKakaoTalkLoginAvailable(context)
+    }
+
+    private fun onSignInSuccess(token: OAuthToken) = viewModelScope.launch(Dispatchers.IO) {
+        _result.update { UiState.Success(Unit) }
+        localDataSource.saveAuthTokenInfo(token.toTokenInfo())
+    }
+
+    private fun onSignInFailure(message: String? = null) = viewModelScope.launch(Dispatchers.IO) {
+        _result.update { UiState.Error(message ?: "로그인에 실패하였습니다") }
+    }
+
+    private fun OAuthToken.toTokenInfo(): TokenInfo {
+        return TokenInfo(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+        )
     }
 }
--- app/src/main/kotlin/com/kick/npl/ui/app/NPLBottomNavBar.kt
+++ app/src/main/kotlin/com/kick/npl/ui/app/NPLBottomNavBar.kt
@@ -2,9 +2,7 @@ package com.kick.npl.ui.app
 
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
